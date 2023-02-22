e0a2a90d889ee7e055242203058831d540b9a1dc
kesavan <kesavanplk7@gmail.com>
2023-02-22 12:57:25 +0530

    functions and arrays

--- script/script.js
+++ script/script.js
@@ -196,13 +196,17 @@ if (
 //coding challenge4
 
 let bill = 275;
-let tip = bill * 0.15;
-let tipValue = bill * 0.2;
+let tipBetween50_300 = bill * 0.15;
+let tipForOther = bill * 0.2;
 if (bill >= 50 && bill <= 300) {
-  console.log(`bill value ${bill} tip ${tip} total bill ${bill + tip}`);
+  console.log(
+    `bill value ${bill} tip ${tipBetween50_300} total bill ${
+      bill + tipBetween50_300
+    }`
+  );
 } else {
   console.log(
-    `bill value ${bill} tip ${tipValue} total bill ${bill + tipValue}`
+    `bill value ${bill} tip ${tipForOther} total bill ${bill + tipForOther}`
   );
 }
 
@@ -210,13 +214,192 @@ if (bill >= 50 && bill <= 300) {
 bill = 600;
 
 bill >= 50 && bill <= 300
-  ? console.log(`bill value ${bill} tip ${tip} total bill ${bill + tip}`)
+  ? console.log(
+      `bill value ${bill} tip ${tipBetween50_300} total bill ${
+        bill + tipBetween50_300
+      }`
+    )
   : console.log(
-      `bill value ${bill} tip ${tipValue} total bill ${bill + tipValue}`
+      `bill value ${bill} tip ${tipForOther} total bill ${bill + tipForOther}`
     );
 
-// if ("55" > "49") {
-//   console.log(true);
-// } else {
-//   console.log(false);
-// }
+//switch case
+
+//use switch case above four conditions
+//switch case needs condition variable
+//break keyword is used in switch to getaway .it doesn't execute following codes.
+
+let SpeakingLanguage; //prompt("language");
+
+switch (SpeakingLanguage) {
+  case "hindhi":
+    console.log(`your language is top 1st`);
+    break;
+  case "tamil":
+    console.log(`your language is top 2nd`);
+    break;
+  case "telugu":
+    console.log(`your language is top 3rd`);
+    break;
+  case "malayalam":
+    console.log(`your language is top 4th`);
+    break;
+  default:
+    console.log(`Your language is not in top4`);
+}
+
+//FUNCTION
+
+//function declaration
+function describeCountry(country, population, capitalCity) {
+  return `${country} has ${population} million people and it's capital city is ${capitalCity}`;
+}
+
+const finlandCountry = describeCountry("finland", 6, "helsinki");
+const indiaCountry = describeCountry("India", 240, "Newdelhi");
+const americaCountry = describeCountry("America", 40, "Newyork");
+
+console.log(finlandCountry);
+console.log(indiaCountry);
+console.log(americaCountry);
+
+function percentageOfWorld1(population) {
+  return `your country population is ${((population / 7900) * 100).toFixed(
+    1
+  )}%`;
+}
+
+const indiaPercentage = percentageOfWorld1(2400);
+const americaPercentage = percentageOfWorld1(400);
+const finlandPercentage = percentageOfWorld1(60);
+
+console.log(indiaPercentage, americaPercentage, finlandPercentage);
+
+//Function expression or Statement
+
+const percentageOfWorld2 = function (population) {
+  return `your country population is ${((population / 7900) * 100).toFixed(
+    1
+  )}%`;
+};
+
+let chinaPercentage = percentageOfWorld2(1400);
+let pakistanPercentage = percentageOfWorld2(800);
+let srilankaPercentage = percentageOfWorld2(120);
+
+console.log(chinaPercentage, pakistanPercentage, srilankaPercentage);
+
+//Arrow function  (statement or expression function)
+
+//explicit type
+let percentageOfWorld3 = (population) => {
+  return `your country population is ${((population / 7900) * 100).toFixed(
+    1
+  )}%`;
+};
+
+chinaPercentage = percentageOfWorld2(3400);
+pakistanPercentage = percentageOfWorld2(2400);
+srilankaPercentage = percentageOfWorld2(899);
+
+console.log(chinaPercentage, pakistanPercentage, srilankaPercentage);
+
+const describePopulation = (country, population) => {
+  return `${country} has ${population} million people, which is about ${percentageOfWorld1(
+    population
+  )}`;
+};
+
+const indiaPopulationAndPercent = describePopulation("india", 2400);
+const chinaPopulationAndPercent = describePopulation("china", 1900);
+const americaPopulationAndPercent = describePopulation("america", 800);
+
+console.log(indiaPopulationAndPercent);
+console.log(chinaPopulationAndPercent);
+console.log(americaPopulationAndPercent);
+
+//coding challenge5
+// Data 1: Dolphins score 44, 23 and 71. Koalas score 65, 54 and 49
+//  Data 2: Dolphins score 85, 54 and 41. Koalas score 23, 34 and 27
+
+let calcAvg = (score1, score2, score3) => (score1 + score2 + score3) / 3;
+
+let avgScoreFfDolphins = calcAvg(44, 23, 71);
+let avgScoreOfKoalas = calcAvg(65, 54, 49);
+
+let checkWinner = (avgScoreFfDolphins, avgScoreOfKoalas) => {
+  if (avgScoreFfDolphins >= avgScoreOfKoalas * 2) {
+    console.log(`Dolphin's won(${avgScoreFfDolphins} vs ${avgScoreOfKoalas}) `);
+  } else if (avgScoreFfDolphins >= avgScoreOfKoalas * 2) {
+    console.log(`Koala's won(${avgScoreOfKoalas} vs ${avgScoreFfDolphins}) `);
+  } else {
+    console.log(`no result`);
+  }
+};
+checkWinner(avgScoreFfDolphins, avgScoreOfKoalas);
+avgScoreFfDolphins = calcAvg(85, 54, 41);
+avgScoreOfKoalas = calcAvg(23, 34, 27);
+checkWinner(avgScoreFfDolphins, avgScoreOfKoalas);
+
+//ARRAY
+
+let populationOfcountry = [2400, 1800, 900, 60];
+
+console.log(populationOfcountry.length === 4);
+
+let percentages = [2400, 1800, 900, 60];
+
+console.log(percentageOfWorld1(percentages[0]));
+console.log(percentageOfWorld1(percentages[1]));
+console.log(percentageOfWorld1(percentages[2]));
+console.log(percentageOfWorld1(percentages[3]));
+
+//array methods
+
+let neighbours = ["pakistan", "china", "srilanka", "bangladesh"];
+console.log(neighbours);
+
+console.log(neighbours.push("nepal")); //it will return array index value
+console.log(neighbours);
+console.log(neighbours.pop()); //it will return popedup array value
+console.log(neighbours);
+
+if (neighbours.includes("germany")) {
+  console.log("Probably a central European country :D");
+}
+console.log("Probably not a central European country :D");
+
+console.log(neighbours.indexOf("bangladesh"));
+let indexOfNameChangingCountry = neighbours.indexOf("bangladesh");
+neighbours[indexOfNameChangingCountry] = "banglatigers";
+console.log(neighbours);
+
+// coding challenge6
+
+let calcTip = (billvalue) => {
+  if (billvalue >= 50 && billvalue <= 300) {
+    console.log(
+      `bill value ${billvalue} tip ${billvalue * 0.15} total bill ${
+        billvalue + billvalue * 0.15
+      }`
+    );
+    return billvalue * 0.15;
+  } else {
+    console.log(
+      `bill value ${billvalue} tip ${billvalue * 0.2} total bill ${
+        billvalue + billvalue * 0.2
+      }`
+    );
+    return billvalue * 0.2;
+  }
+};
+calcTip(100);
+
+let bills = [125, 555, 44];
+let tips = [];
+tips.push(calcTip(bills[0]));
+tips.push(calcTip(bills[1]));
+tips.push(calcTip(bills[2]));
+
+console.log(bills);
+console.log(tips);
